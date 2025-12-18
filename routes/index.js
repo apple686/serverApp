47da77ac7197f58b5d9652dec5b810b51df87ea9
twp <1427257988@qq.com>
2025-12-18 13:54:10 +0800

    feat: 1

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   // res.render('index', { title: 'Express' });
-  return {
+  res.json({
     id: 1
-  };
+  });
 });
 
 module.exports = router;
